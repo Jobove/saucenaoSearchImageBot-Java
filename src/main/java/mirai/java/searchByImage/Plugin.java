5c900d0129f75c10eb814ef292220358dee503b3
Jobove <OYCuBa@gmail.com>
2021-10-05 15:40:54 +0800

    Add similarity threshold.

--- src/main/java/mirai/java/searchByImage/Plugin.java
+++ src/main/java/mirai/java/searchByImage/Plugin.java
@@ -1,21 +1,27 @@
 package mirai.java.searchByImage;
 
-import java.io.*;
-import java.nio.charset.StandardCharsets;
-import java.util.Set;
-import java.util.regex.*;
-import com.alibaba.fastjson.*;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.annotation.JSONField;
 import net.mamoe.mirai.console.plugin.jvm.JavaPlugin;
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescriptionBuilder;
 import net.mamoe.mirai.event.GlobalEventChannel;
 import net.mamoe.mirai.event.Listener;
 import net.mamoe.mirai.event.events.GroupMessageEvent;
-import net.mamoe.mirai.message.data.*;
+import net.mamoe.mirai.message.data.Image;
+import net.mamoe.mirai.message.data.MessageChain;
+import net.mamoe.mirai.message.data.MessageChainBuilder;
+import net.mamoe.mirai.message.data.QuoteReply;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.net.*;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public final class Plugin extends JavaPlugin {
     public static final Plugin INSTANCE = new Plugin();
@@ -35,22 +41,39 @@ public final class Plugin extends JavaPlugin {
     public void onEnable() {
         getLogger().info("ImageSearching bot loaded!");
         settings = checkSettings();
-        if(settings != null)
-            listener = GlobalEventChannel.INSTANCE.subscribeAlways(GroupMessageEvent.class, groupMessageEvent -> {
-            MessageChain chain = groupMessageEvent.getMessage();
-            String messageString = chain.contentToString();
-            String re = "以图搜图";
-            Pattern pattern = Pattern.compile(re, Pattern.MULTILINE);
-            Matcher matcher = pattern.matcher(messageString);
+        if (settings != null)
+            listener = GlobalEventChannel.INSTANCE.subscribeAlways(GroupMessageEvent.class,
+                    groupMessageEvent -> {
+                        MessageChain chain = groupMessageEvent.getMessage();
+                        String messageString = chain.contentToString();
+                        String reSimp = "以图搜图[ \\n]\\[图片]";
+                        String reComp = "以图搜图[ \\n]\\[图片][ \\n](\\d\\d?\\.\\d\\d?|\\d\\d?|)";
+                        Pattern patternSimp = Pattern.compile(reSimp, Pattern.MULTILINE | Pattern.DOTALL);
+                        Matcher matcherSimp = patternSimp.matcher(messageString);
+                        Pattern patternComp = Pattern.compile(reComp, Pattern.MULTILINE | Pattern.DOTALL);
+                        Matcher matcherComp = patternComp.matcher(messageString);
 
-            if (matcher.find()) {
-                Image image = (Image) chain.stream().filter(Image.class::isInstance).findFirst().orElse(null);
-                if (image != null) {
-                    MessageChain messageChain = new MessageChainBuilder().append(new QuoteReply(groupMessageEvent.getSource())).append(image).append(searchBySaucenao(Image.queryUrl(image))).build();
-                    groupMessageEvent.getGroup().sendMessage(messageChain);
-                }
-            }
-        });
+                        if (matcherComp.find()) {
+                            Image image = (Image) chain.stream().filter(Image.class::isInstance).findFirst().orElse(null);
+                            double threshold = Double.parseDouble(matcherComp.group(1));
+                            if (image != null) {
+                                MessageChain messageChain = new MessageChainBuilder()
+                                        .append(new QuoteReply(groupMessageEvent.getSource()))
+                                        .append(image).append(searchBySaucenao(Image.queryUrl(image), threshold))
+                                        .build();
+                                groupMessageEvent.getGroup().sendMessage(messageChain);
+                            }
+                        } else if (matcherSimp.find()) {
+                            Image image = (Image) chain.stream().filter(Image.class::isInstance).findFirst().orElse(null);
+                            if (image != null) {
+                                MessageChain messageChain = new MessageChainBuilder()
+                                        .append(new QuoteReply(groupMessageEvent.getSource()))
+                                        .append(image).append(searchBySaucenao(Image.queryUrl(image), 80.0)).build();
+
+                                groupMessageEvent.getGroup().sendMessage(messageChain);
+                            }
+                        }
+                    });
     }
 
     private String getHttpResult(String queryUrl) {
@@ -102,31 +125,35 @@ public final class Plugin extends JavaPlugin {
     }
 
     @NotNull
-    private String searchBySaucenao(String imageUrl) {
+    private String searchBySaucenao(String imageUrl, double threshold) {
         String apiKey = settings.getApiKey();
         String queryUrl = "https://saucenao.com/search.php?db=999&output_type=2&numres=16&api_key=" + apiKey + "&url=" + imageUrl;
         String result = getHttpResult(queryUrl);
         StringBuilder returnString = new StringBuilder();
-        boolean hasAnswer = false;
-
         JSONObject jsonObject = JSON.parseObject(result);
         JSONArray resultArray = jsonObject.getJSONArray("results");
+
+        int answerCount = 0;
         for (int i = 0; i < resultArray.size(); ++i) {
-//            JSONObject resultItem = JSON.parseObject(o.toString());
+
             JSONObject resultItem = resultArray.getJSONObject(i);
             JSONArray urlArray = resultItem.getJSONObject("data").getJSONArray("ext_urls");
             double similarity = resultItem.getJSONObject("header").getDouble("similarity");
-            if(urlArray == null)
+
+            if (urlArray == null)
                 continue;
-            if (similarity < 80)
+            if (similarity < threshold)
                 break;
-            if(!hasAnswer) {
+            if(answerCount >= 5)
+                break;
+
+            if (answerCount == 0) {
                 returnString.append("在以下来源找到相似度大于80%的结果:\n");
-                hasAnswer = true;
             }
             returnString.append("相似度：").append(similarity).append("，链接：").append(urlArray.getString(0)).append('\n');
+            ++answerCount;
         }
-        if(!hasAnswer) {
+        if (answerCount == 0) {
             returnString.append("未在任何来源中寻找到相似度大于80%的结果。");
         }
         return returnString.toString();
@@ -138,14 +165,14 @@ public final class Plugin extends JavaPlugin {
 
         //如果设置文件所在文件夹不存在, 则生成
         File settingsFolder = new File("config/searchByImage");
-        if(!settingsFolder.exists() || !settingsFolder.isDirectory()) {
+        if (!settingsFolder.exists() || !settingsFolder.isDirectory()) {
             returnValue = settingsFolder.mkdirs();
             getLogger().warning("Making directory");
         }
 
         //如果设置文件不存在, 则生成, 并写入默认的空设置
         File settingFile = new File("config/searchByImage/config.json");
-        if(!settingFile.exists() || !settingFile.isFile()) {
+        if (!settingFile.exists() || !settingFile.isFile()) {
             try {
                 getLogger().warning("Creating config file");
                 returnValue = settingFile.createNewFile();
@@ -165,27 +192,26 @@ public final class Plugin extends JavaPlugin {
         try {
             FileInputStream configFileInputStream = new FileInputStream(settingFile);
             InputStreamReader configFileInputStreamReader = new InputStreamReader(configFileInputStream);
-            while(configFileInputStreamReader.ready()) {
+            while (configFileInputStreamReader.ready()) {
                 configStringBuilder.append((char) configFileInputStreamReader.read());
             }
             configFileInputStreamReader.close();
             configFileInputStream.close();
-        } catch(IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
         String configString = configStringBuilder.toString();
 
         Settings settings = JSON.parseObject(configString, Settings.class);
 
-        if(settings.getApiKey().length() != 40) {
+        if (settings.getApiKey().length() != 40) {
             getLogger().warning("程序配置文件错误, ApiKey长度不等于40!");
             returnValue = false;
         }
 
-        if(returnValue) {
+        if (returnValue) {
             return settings;
-        }
-        else {
+        } else {
             return null;
         }
     }
